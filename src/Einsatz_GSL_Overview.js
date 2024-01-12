71e611958ac8026d7c00465f683ed8a29b8a0493
FunaFuitBar <156161268+FunaFuitBar@users.noreply.github.com>
2024-01-12 07:22:52 +0100

    Update Einsatz_GSL_Overview.js

--- src/Einsatz_GSL_Overview.js
+++ src/Einsatz_GSL_Overview.js
@@ -1,4 +1,14 @@
- 'use strict';
+// ==UserScript==
+// @name         Einsatz und GSL overview
+// @version      1.0.0
+// @author       TheWatcher - Watch at me!
+// @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/$/
+// @grant        GM_addStyle
+// ==/UserScript==
+/* global $ */
+
+(function() {
+    'use strict';
 
     GM_addStyle(`.modal {
 display: none;
